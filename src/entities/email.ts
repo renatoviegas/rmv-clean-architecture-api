9ae96e703c7d2415852b7a2a98992e53655c32b6
Renato Viegas <renato.viegas@hotmail.com>
2023-08-12 14:11:57 -0300

    feat: add email validations

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,7 +1,51 @@
 export class Email {
+  private static MAX_LENGHT_LOCAL_PART_CHARS = 64
+  private static MAX_LENGHT_DOMAIN_PART_CHARS = 63
+  private static MAX_LENGHT_DOMAIN_CHARS = 255
+  private static MAX_LENGHT_TOTAL_CHARS = 320
+  private static EMAIL_REGEX = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+
+  private static _validateIfEmailIsNullOrEmpty(email: string) {
+    if (!email) throw Error()
+  }
+
+  private static _validateEmailRegex(email: string) {
+    if (!this.EMAIL_REGEX.test(email)) throw Error()
+  }
+
+  private static _validateMaxLenght(email: string) {
+    if (email.length > this.MAX_LENGHT_TOTAL_CHARS) throw Error()
+  }
+
+  private static _validateIfLocalAndDomainIsEmpty(email: string) {
+    const [local, domain] = email.split('@')
+
+    if (!local || !domain) throw Error()
+  }
+
+  private static _validateMaxLocalAndDomainLenght(email: string) {
+    const [local, domain] = email.split('@')
+
+    if (local.length > this.MAX_LENGHT_LOCAL_PART_CHARS) throw Error()
+    if (domain.length > this.MAX_LENGHT_DOMAIN_CHARS) throw Error()
+
+    const domainParts = domain.split('.')
+
+    if (domainParts.some(part => part.length > this.MAX_LENGHT_DOMAIN_PART_CHARS)) throw Error()
+  }
 
   static validate(email: string): boolean {
-    return !!email;
+    try {
+      this._validateIfEmailIsNullOrEmpty(email)
+      this._validateEmailRegex(email)
+      this._validateMaxLenght(email)
+      this._validateIfLocalAndDomainIsEmpty(email)
+      this._validateMaxLocalAndDomainLenght(email)
+
+      return true
+    } catch (error) {
+      return false;
+    }
   }
 
 }
--- test/entities/email-validation.test.ts
+++ test/entities/email-validation.test.ts
@@ -1,21 +1,75 @@
 import { Email } from "../../src/entities/email"
 
 describe('Email validation', () => {
-  test('should not accept null strings', () => {
-    const email = null
+  describe('Cases valids', () => {
+    test('should accept valid email', () => {
+      const email = 'any@mail.com'
 
-    expect(Email.validate(email)).toBeFalsy()
+      expect(Email.validate(email)).toBeTruthy()
+    })
   })
 
-  test('should not accept empty strings', () => {
-    const email = ''
+  describe('Cases invalids', () => {
+    test('should not accept null strings', () => {
+      const email = null
 
-    expect(Email.validate(email)).toBeFalsy()
-  })
+      expect(Email.validate(email)).toBeFalsy()
+    })
 
-  test('should accept valid email', () => {
-    const email = 'any@mail.com'
+    test('should not accept empty strings', () => {
+      const email = ''
 
-    expect(Email.validate(email)).toBeTruthy()
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept strings large than 320 chars', () => {
+      const email = `any@${'a'.repeat(320)}.com`
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept local part larger than 64 chars', () => {
+      const email = `${'l'.repeat(65)}@mail.com`
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept domain part large than 255 chars', () => {
+      const email = `any@${'a'.repeat(256)}.com`
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept empty local part', () => {
+      const email = '@mail.com'
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept empty domain part', () => {
+      const email = 'any@'
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept domain with a part larger than 63 chars', () => {
+      const email = `any@${'d'.repeat(64)}.com`
+
+      expect(Email.validate(email)).toBeFalsy()
+    })
+
+    test('should not accept local part with invalid char', () => {
+      const emailWithSpace = 'any mail@mail.com'
+      const emailWithoutAtSign = 'any'
+      const emailThatLocalPartEndingWithDot = 'any.@mail.com'
+      const emailHasTwoDot = 'any@mail..com'
+      const emailHasOthersInvalidsChars = 'a(3@a.com'
+
+      expect(Email.validate(emailWithSpace)).toBeFalsy()
+      expect(Email.validate(emailWithoutAtSign)).toBeFalsy()
+      expect(Email.validate(emailThatLocalPartEndingWithDot)).toBeFalsy()
+      expect(Email.validate(emailHasTwoDot)).toBeFalsy()
+      expect(Email.validate(emailHasOthersInvalidsChars)).toBeFalsy()
+    })
   })
 })
